5d65e8ab83013900b7492aea55e7ed2269f3f009
Drorre55 <dror604604@gmail.com>
2024-04-18 14:19:35 +0300

    added searcher

---
+++ MamaSearcher/IMamaSearcher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MamaSearcher
+{
+    public interface IMamaSearcher
+    {
+        void Subscribe(string pattern, Action<int, string> actionToPerform);
+        void PerformSearch(string content);
+    }
+}
---
+++ MamaSearcher/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MamaSearcher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Searcher searcher = new Searcher();
+            searcher.Subscribe("test", PrintResult);
+            searcher.PerformSearch("Hello, World!testgarbage");
+        }
+
+        static void PrintResult(int index, string pattern)
+        {
+            string text = $"{index}: {pattern}";
+            Console.WriteLine(text);
+        }
+    }
+}
